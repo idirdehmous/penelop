e17fe3b9e05e4bf531321e4fb1c76a9fdfac9597
Idir Dehmous <idehmous@meteo.be>
2026-08-09 17:27:15 +0200

    App needed plusgins

---
+++ plugins/get_plugins.sh
@@ -0,0 +1,11 @@
+ curl  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"      --output  leaflet.js
+ curl  "https://d3js.org/d3.v7.min.js"      --output    d3.v7.min.js
+ curl  "https://cdn.jsdelivr.net/npm/geotiff/dist-browser/geotiff.min.js"  --output  geotiff.min.js
+ curl  "https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.4.2/chroma.min.js" --output  chroma.min.js
+ curl  "https://cdn.jsdelivr.net/npm/leaflet-canvaslayer-field@1.4.1/dist/leaflet.canvaslayer.field.js" --output   leaflet.canvaslayer.field.js
+ curl  "https://unpkg.com/georaster" --output  georaster
+ curl  "https://unpkg.com/georaster-layer-for-leaflet/dist/georaster-layer-for-leaflet.min.js" --output  georaster-layer-for-leaflet.min.js
+ curl  "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/7.4.0/pixi.min.js"  --output  pixi.min.js 
+ curl  "https://unpkg.com/leaflet-pixi-overlay/L.PixiOverlay.min.js"   --output  L.PixiOverlay.min.js
+ curl  "https://code.jquery.com/jquery-3.7.1.min.js"    --output   jquery-3.7.1.min.js
+
